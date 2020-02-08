5bfc86a5bd5bef346f1c218d95d06203942cfb0f
zhaojishun <1207296994@qq.com>
2020-02-08 16:49:06 +0800

    单例模式学习

---
+++ javaBase/src/main/java/cn/zhaojishun/javaBase/设计模式/单例模式/懒汉式/线程安全/Singleton.java
@@ -0,0 +1,25 @@
+package cn.zhaojishun.javaBase.设计模式.单例模式.懒汉式.线程安全;
+
+/**
+ * @program: javaBaseLearn
+ * @description:
+ * @author: Shunji Zhao
+ * @create: 2020-02-08 16:26
+ */
+public class Singleton {
+
+    private static volatile Singleton singleton;
+
+    private Singleton(){}
+
+    public static synchronized Singleton getInstance(){
+        if(singleton == null){
+            synchronized (Singleton.class){
+                if (singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
